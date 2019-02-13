25a23a1d5c8fa64626e134d07ee78f7cf24ec57c
Andrii Romanchak <Andrii_Romanchak@epam.com>
2019-02-13 19:03:40 +0200

    added starting functions

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,15 @@
-console.log('Hello from popup!');
+const inputElement = document.getElementById('search-input');
+const suggestionsElement = document.getElementById('suggestions');
+
+function init() {
+    inputElement.focus();
+    inputElement.addEventListener('input', event => {
+        updateSuggestions(event.target.value);
+    });
+}
+
+function updateSuggestions(query) {
+    suggestionsElement.innerHTML = query;
+}
+
+init();
