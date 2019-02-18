f42d6ff1b67d73bc35ce6e1d9a18c7ad14e608db
Andrii Romanchak <Andrii_Romanchak@epam.com>
2019-02-19 01:20:18 +0200

    minor refactoring

--- src/app/models/bookmark-node-suggestion.ts
+++ src/app/models/bookmark-node-suggestion.ts
@@ -2,5 +2,19 @@ export class BookmarkNodeSuggestion {
   constructor(
     public bookmarkNode: chrome.bookmarks.BookmarkTreeNode,
     public domElement: HTMLElement
-  ) {}
+  ) {
+    // todo init event listeners
+  }
+
+  public select() {
+
+  }
+
+  public deselect() {
+
+  }
+
+  public createSubNode() {
+
+  }
 }
